ce501bd3a8c1cf75d9d86e0b8f672951572b4119
user <Leein05.07.28@gmail.com>
2021-12-19 00:11:38 +0900

    #디버그를 hi로 변경

---
+++ src/main/java/kr/kro/narileein05/kid/functions/events/AlwaysEvents.java
@@ -0,0 +1,14 @@
+package kr.kro.narileein05.kid.functions.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class AlwaysEvents implements Listener {
+
+    @EventHandler
+    public void test(PlayerInteractEvent e) {
+        e.getPlayer().sendMessage("hi");
+    }
+}
