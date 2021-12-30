9964ea2ae321b3bd6412f23c52061aef4d89a0e6
user <Leein05.07.28@gmail.com>
2021-12-30 09:58:01 +0900

    수정

--- src/main/java/kr/kro/narileein05/kid/functions/events/Dev.java
+++ src/main/java/kr/kro/narileein05/kid/functions/events/Dev.java
@@ -1,14 +1,16 @@
 package kr.kro.narileein05.kid.functions.events;
 
 import kr.kro.narileein05.kid.functions.events.New.JoinEvents;
-import kr.kro.narileein05.kid.functions.events.New.QuitEvent;
+import kr.kro.narileein05.kid.functions.events.New.QuitEvents;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.desktop.QuitEvent;
 
 public class Dev extends JavaPlugin {
     @Override
     public void onEnable() {
         //Plugin start logic
         getServer().getPluginManager().registerEvents(new JoinEvents(), this);
-        getServer().getPluginManager().registerEvents(new QuitEvent(), this);
+        getServer().getPluginManager().registerEvents(new QuitEvents(), this);
     }
 }
--- src/main/java/kr/kro/narileein05/kid/functions/events/New/QuitEvents.java
+++ src/main/java/kr/kro/narileein05/kid/functions/events/New/QuitEvents.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class QuitEvent implements Listener {
+public class QuitEvents implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
